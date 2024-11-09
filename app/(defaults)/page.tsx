9d622aecf2f35f119dede00fe1ddea31a821844e
nashad <mail2nsdoff@gmail.com>
2024-11-08 21:23:45 -0500

    build fix 1

--- app/(defaults)/page.tsx
+++ app/(defaults)/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from 'next';
 import { useRouter } from 'next/navigation';
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 
 export const metadata: Metadata = {
     title: 'CLAZZY TR',
