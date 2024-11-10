97a7fee499e65572939e62c6303abfaf66d7dac9
nashad <mail2nsdoff@gmail.com>
2024-11-09 20:27:53 -0500

    netlify redirects and toml files bug fix 2

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,25 @@
-// method 1
-import type { NextRequest } from 'next/server';
+import { MiddlewareRequest, NextRequest } from '@netlify/next';
 import { NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest) {
-    const token = req.cookies.get('auth_token'); // Get auth token from cookies
+    const request = new MiddlewareRequest(req);
+    const token = req.cookies.get('auth_token');
 
     const url = req.nextUrl.clone();
+    const protectedRoutes = ['/dashboard', '/permissions', '/roles', '/users', '/enquiries'];
 
-    // Protect the routes by checking if the user is logged in (token exists)
-    if (!token) {
-        // const protectedRoutes = ['/', '/dashboard', '/permissions', '/roles', '/users', '/enquiries']; // Add more protected routes here
-        const protectedRoutes = ['/dashboard', '/permissions', '/roles', '/users', '/enquiries']; // Add more protected routes here
-        const shouldProtectRoute = protectedRoutes.some((route) => url.pathname.startsWith(route));
+    const isProtectedRoute = protectedRoutes.some((route) => url.pathname.startsWith(route));
 
-        if (shouldProtectRoute) {
-            url.pathname = '/auth/login'; // Redirect to login page if not authenticated
-            return NextResponse.redirect(url);
-        }
+    // Redirect if accessing protected routes without a valid token
+    if (isProtectedRoute && !token) {
+        url.pathname = '/auth/login';
+        return NextResponse.redirect(url);
     }
 
-    return NextResponse.next(); // Allow the request to proceed if authenticated
+    // If token exists, proceed with request as usual
+    return request.next();
 }
 
 export const config = {
-    // matcher: ['/', '/dashboard', '/permissions', '/roles', '/users', '/enquiries', '/enquiries/:path*', '/users/:path*'], // dynamic matching
-    matcher: ['/dashboard', '/permissions', '/roles', '/users', '/enquiries', '/enquiries/:path*', '/users/:path*'], // dynamic matching
+    matcher: ['/dashboard', '/permissions', '/roles', '/users', '/enquiries'],
 };
---
+++ old-next-middleware.ts
@@ -0,0 +1,26 @@
+// method 1
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+export async function middleware(req: NextRequest) {
+    const token = req.cookies.get('auth_token'); // Get auth token from cookies
+
+    const url = req.nextUrl.clone();
+
+    // Protect the routes by checking if the user is logged in (token exists)
+    if (!token) {
+        const protectedRoutes = ['/', '/dashboard', '/permissions', '/roles', '/users', '/enquiries']; // Add more protected routes here
+        const shouldProtectRoute = protectedRoutes.some((route) => url.pathname.startsWith(route));
+
+        if (shouldProtectRoute) {
+            url.pathname = '/auth/login'; // Redirect to login page if not authenticated
+            return NextResponse.redirect(url);
+        }
+    }
+
+    return NextResponse.next(); // Allow the request to proceed if authenticated
+}
+
+export const config = {
+    matcher: ['/', '/dashboard', '/permissions', '/roles', '/users', '/enquiries', '/enquiries/:path*', '/users/:path*'], // dynamic matching
+};
